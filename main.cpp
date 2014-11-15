06c383e5fbcd7a4d3959e1137507d6a277e2cc3a
Aetf <7437103@gmail.com>
2014-11-16 01:28:05 +0800

    main: small test case

--- main.cpp
+++ main.cpp
@@ -1,19 +1,15 @@
 #include <QCoreApplication>
-#include <QVariant>
 #include <QDebug>
-#include <QJsonArray>
-#include <QJsonObject>
-#include <QJsonValue>
-#include <QJsonDocument>
+#include "qbencodedocument.h"
 
 using std::endl;
 int main(int argc, char *argv[])
 {
-    qint64 li = 123;
-    QVariant data(li);
-    QJsonDocument doc;
-    qDebug() << "doc isEmpty:" << doc.isEmpty() << " isNull:" << doc.isNull();
-    qDebug() << data << endl;
-    qDebug() << data.type() << endl;
+    QByteArray bencode("li12e4:abcde");
+    QBencodeParseError err;
+    QBencodeDocument doc = QBencodeDocument::fromBencode(bencode, &err);
+
+    qDebug() << err.errorString();
+    qDebug() << doc;
     return 0;
 }
