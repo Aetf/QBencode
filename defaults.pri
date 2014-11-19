02d56c28405c96f1659ccf8f40be74d4bbdfc9a5
Aetf <7437103@gmail.com>
2014-11-19 18:44:46 +0800

    Build: enough qmake! let's change to cmake

--- defaults.pri
+++
@@ -1,4 +0,0 @@
-INCLUDEPATH += $$PWD/src $$PWD/src/include
-SRC_DIR = $$PWD
-
-CONFIG += c++11
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LD_LIBRARY_PATH=${DIR}/../src ${DIR}/tests 1>&2
+LD_LIBRARY_PATH=${DIR}/../lib ${DIR}/tests 1>&2
